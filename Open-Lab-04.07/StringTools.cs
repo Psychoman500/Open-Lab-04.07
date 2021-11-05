c7cf3f98fcfd2d63d30408690d2714aa786acbdf
Psychoman500 <martin.sutek@gmail.com>
2021-11-05 07:51:11 +0100

    Reverse string.

--- Open-Lab-04.07/StringTools.cs
+++ Open-Lab-04.07/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._07
 {
@@ -6,7 +7,7 @@ namespace Open_Lab_04._07
     {
         public string Reverse(string original)
         {
-            throw new NotImplementedException();
+            return new string(original.Reverse().ToArray());
         }
     }
 }
